18193116010fcc2188a49608f6c8da54567b56a8
kobepeng <yipeng.info@gmail.com>
2022-04-24 17:02:30 +0800

    refactor: change api vendor to nomic

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const Koa = require('koa');
 const startTask = require('./schedule');
 
-const app = new Koa()
+const app = new Koa();
 
 app.on('error', (err, ctx) => {
     console.log('ctx: ', ctx);
     ctx.status = 401
     ctx.body = err.message
-})
+});
 
 app.listen(7007, () => {
     startTask();
     console.log('server start');
-})
+});
